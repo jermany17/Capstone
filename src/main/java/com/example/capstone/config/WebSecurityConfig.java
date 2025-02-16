c9a7466be94b4fd9ca2fdb56e21a175194e1f1ea
jermany17 <leejm5050@naver.com>
2025-02-16 15:03:07 +0900

    불필요한 import 제거

--- src/main/java/com/example/capstone/config/WebSecurityConfig.java
+++ src/main/java/com/example/capstone/config/WebSecurityConfig.java
@@ -11,10 +11,8 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
