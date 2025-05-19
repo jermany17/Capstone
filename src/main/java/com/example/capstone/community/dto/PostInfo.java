b0718b7a67c2c250bc688c506dbb62c0632b7913
jermany17 <leejm5050@naver.com>
2025-05-19 15:11:48 +0900

    댓글 userId 추가 반환

--- src/main/java/com/example/capstone/community/dto/PostInfo.java
+++ src/main/java/com/example/capstone/community/dto/PostInfo.java
@@ -39,12 +39,14 @@ public class PostInfo {
     @Getter
     public static class CommentDto {
         private final Long id;
+        private final String userId;
         private final String userName;
         private final String content;
         private final LocalDateTime createAt;
 
         public CommentDto(Comment comment) {
             this.id = comment.getId();
+            this.userId = comment.getUserId();
             this.userName = comment.getUserName();
             this.content = comment.getContent();
             this.createAt = comment.getCreateAt();
